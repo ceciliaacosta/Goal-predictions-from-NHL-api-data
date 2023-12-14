85ed602830b6fffa2aa444100aa89db4c26f813d
beafra08 <114113197+beafra08@users.noreply.github.com>
2023-12-14 15:42:10 -0500

    Update run.sh

--- docker-project-template-main/run.sh
+++ docker-project-template-main/run.sh
@@ -1,2 +1,5 @@
-#Docker run command
-docker run -p 8050:8050 -e COMET_API_KEY="eeuSdrSn1FqEQ6e7jtLRmlnFt" hockey-app
+# Retrieve the Comet API key from the local machine environment variable
+COMET_API_KEY=${COMET_API_KEY}
+
+# Run the Docker container with the dynamically retrieved API key
+docker run -p 8050:8050 -e COMET_API_KEY="${COMET_API_KEY}" hockey-app
