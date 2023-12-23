25845c32f02adc8ecc99f75790f47f65785d7e3c
F.Fatima zahra <fatima.zahra.fawzi@umontreal.ca>
2023-12-23 09:16:58 -0500

    Update serving_client.py

--- docker-project-template-main/ift6758/ift6758/client/serving_client.py
+++ docker-project-template-main/ift6758/ift6758/client/serving_client.py
@@ -2,13 +2,13 @@ import json
 import requests
 import pandas as pd
 import logging
+import os
 
 
 logger = logging.getLogger(__name__)
 
-
 class ServingClient:
-    def __init__(self, ip: str = "0.0.0.0", port: int = 5000, features=None):
+    def __init__(self, ip: str = "0.0.0.0", port: int = 5050, features=None):
         self.base_url = f"http://{ip}:{port}"
         logger.info(f"Initializing client; base URL: {self.base_url}")
 
@@ -27,18 +27,28 @@ class ServingClient:
         Args:
             X (Dataframe): Input dataframe to submit to the prediction service.
         """
-        url = f"{self.base_url}/predict"
         #data = json.dumps([X])
-        selected_data = X[self.features]
-        data = selected_data.to_json(orient='records')
-        #data_dict = json.loads(data)
-        #data = selected_data.to_json(orient='records')
-        response = requests.post(url, json=data)
-        response_data = response.json()
-        if 'predictions' in response_data:
-            return pd.DataFrame(response_data['predictions'])
-        else:
-            raise ValueError(f"Unexpected response format: {response_data}")
+        url = f"{self.base_url}/predict"
+        payload = X.to_json(orient='records')
+        headers = {'Content-Type': 'application/json'}
+        try:
+            response = requests.post(url, json=payload)
+            #response_data = response.json()
+            #response.raise_for_status()
+            response_data = response.json()
+
+            # Vérifiez d'abord si la réponse est un succès et contient des prédictions
+            if response_data['status'] == 'success' and 'predictions' in response_data:
+            # Créez un DataFrame à partir de la liste de prédictions
+                predictions = pd.DataFrame(response_data['predictions'], columns=['prediction'])
+                
+            else:
+                 # Gérez le cas où la réponse n'est pas ce que vous attendiez
+                 raise ValueError(f"Unexpected response format or failure status: {response_data}")
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Request to {url} failed: {e}")
+            raise
+        return predictions
 
     def logs(self) -> dict:
         """Get server logs"""
@@ -74,33 +84,33 @@ class ServingClient:
         #raise NotImplementedError("TODO: implement this function")
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    # Create an instance of ServingClient
-    client = ServingClient(ip="127.0.0.1", port=8080, features=["distance_to_target_goal","angle_to_target_goal"])
+#     # Create an instance of ServingClient
+#     client = ServingClient(ip="127.0.0.1", port=8080, features=["distance_to_target_goal","angle_to_target_goal"])
 
-    # Example DataFrame for prediction
-    data = pd.DataFrame({
-    "distance_to_target_goal": [44.6430285711],
-    "angle_to_target_goal": [74.4071890607]})
+#     # Example DataFrame for prediction
+#     data = pd.DataFrame({
+#     "distance_to_target_goal": [44.6430285711],
+#     "angle_to_target_goal": [74.4071890607]})
 
-    # Download a model from the registry
-    model_download_response = client.download_registry_model  ( workspace="francis75",
-        model="Regression3",
-        version="1",)
-    print("Model Download Response:")
-    print(model_download_response)
+#     # Download a model from the registry
+#     model_download_response = client.download_registry_model  ( workspace="francis75",
+#         model="Regression3",
+#         version="1",)
+#     print("Model Download Response:")
+#     print(model_download_response)
 
 
-    # Predict using the model
-    predictions = client.predict(data)
-    print("Predictions:")
-    print(predictions)
+#     # Predict using the model
+#     predictions = client.predict(data)
+#     print("Predictions:")
+#     print(predictions)
 
 
     # Get server logs
-    logs = client.logs()
-    print("Server Logs:")
-    print(logs)
+    # logs = client.logs()
+    # print("Server Logs:")
+    # print(logs)
 
 
